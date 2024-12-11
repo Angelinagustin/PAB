ecfb13047a1d802fb2c5b934fa37c5a70a839faa
Angelinagustin <160854942+Angelinagustin@users.noreply.github.com>
2024-12-11 17:59:15 +0700

    Add: botton
    .

---
+++ Button/flutter_application_1/lib/screens/main_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/business_screen.dart';
+import 'package:flutter_application_1/screens/home_screen.dart';
+import 'package:flutter_application_1/screens/school_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int_selectedIndex = 0;
+  
+  static const List<Widget> _screenOptions =<Widget>[
+    HomeScreen(),
+    BusinessScreen(),
+    SchoolScreen()
+  ];
+  void _onItemTapped(int index){
+    setState(() {
+      int_selectedIndex = index;
+    });
+  }
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+      body: _screenOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem> [
+        //item pertama
+        BottomNavigationBarItem(icon: Icon(Icons.home),label:'Home'),
+        //item ledua
+        BottomNavigationBarItem(icon: Icon(Icons.business), label: 'Business'),
+        //itenm ketiga
+        BottomNavigationBarItem(icon: Icon(Icons.school), label: 'School'),
+      ],
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+      showSelectedLabels: true,
+      showUnselectedLabels: true,
+      selectedItemColor: Colors.amber,
+      unselectedItemColor: Colors.grey,
+      ),
+    );
+  }
+}
