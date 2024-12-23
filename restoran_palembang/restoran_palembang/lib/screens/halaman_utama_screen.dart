4e9c84f619fe6d46a3a911ea1264876c0915313b
Angelinagustin <160854942+Angelinagustin@users.noreply.github.com>
2024-12-23 12:01:37 +0700

    Add: tampilan utama

---
+++ restoran_palembang/restoran_palembang/lib/screens/halaman_utama_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class HalamanUtamaScreen extends StatelessWidget {
+  const HalamanUtamaScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white, // Set the background color to white
+      appBar: AppBar(
+        backgroundColor: Colors.white, // Set the AppBar background to white
+        elevation: 0, // Remove shadow for a clean look
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Logo image
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Image.asset('images/logo-aplikasi-restoran-palembang.png'),
+            ),
+            // Login button
+            const SizedBox(height: 16),
+            SizedBox(
+              width: 150,
+              height: 40,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 235, 113, 0),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            // Sign Up button with the same size and style as the Login button
+            const SizedBox(height: 16),
+            SizedBox(
+              width: 150,
+              height: 40,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 235, 113, 0),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'Sign Up',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
